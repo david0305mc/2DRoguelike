89be1953aec6918dd95468082d04a73d946753ca
david0305mc <david0305mc@gmail.com>
2022-04-06 17:48:18 +0900

    플레이어 컨트롤러 수정

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -28,7 +28,7 @@ public class PlayerController : MonoBehaviour
         }
 
         Vector2 input = playerInput.actions["Move"].ReadValue<Vector2>();
-        Vector3 move = new Vector3(input.x, 0, input.y);
+        Vector3 move = new Vector3(input.x, input.y, 0);
 
         controller.Move(move * Time.deltaTime);
     }
