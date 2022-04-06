0a6190e7f10cfdae59574abf228a169913c82615
david <david0305mc@gmail.com>
2022-04-07 00:29:32 +0900

    Changed Input System

---
+++ Assets/Scripts/Ingame/TankController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankController : MonoBehaviour
+{
+    public Rigidbody2D rb2d;
+    private Vector2 movementVector;
+    public float maxSpeed = 10f;
+    public float rotationSpeed = 100f;
+
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();    
+    }
+
+    public void HandleShoot()
+    {
+        Debug.Log("Shooting");
+    }
+
+    public void HandleMoveBody(Vector2 movementVector)
+    {
+        this.movementVector = movementVector;
+    }
+
+    public void HandleTurretMovement(Vector2 pointerPosition)
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb2d.velocity = (Vector2)transform.up * movementVector.y * maxSpeed * Time.fixedDeltaTime;
+        rb2d.MoveRotation(transform.rotation * Quaternion.Euler(0, 0, -movementVector.x * rotationSpeed * Time.fixedDeltaTime));
+    }
+}
---
+++ Assets/Scripts/Ingame/TankInput.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TankInput : MonoBehaviour
+{
+    [SerializeField] private Camera mainCamera;
+    public UnityEvent OnShoot = new UnityEvent();
+    public UnityEvent<Vector2> OnMoveBody = new UnityEvent<Vector2>();
+    public UnityEvent<Vector2> OnMoveTurret = new UnityEvent<Vector2>();
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetBodyMovemovement();
+        GetTurretMovement();
+        GetShootingInput();
+    }
+
+    private void GetShootingInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            OnShoot?.Invoke();
+        }
+    }
+
+    private void GetTurretMovement()
+    {
+        OnMoveTurret?.Invoke(GetMousePosition());
+    }
+
+    private Vector2 GetMousePosition()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = mainCamera.nearClipPlane;
+        Vector2 mouseWorldPosition = mainCamera.ScreenToWorldPoint(mousePosition);
+        return mouseWorldPosition;
+    }
+
+    private void GetBodyMovemovement()
+    {
+        Vector2 movementVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        OnMoveBody?.Invoke(movementVector.normalized);
+    }
+}
