f0ffae5b70cf5036735fce97ddcd8afd1701e098
david <david0305mc@gmail.com>
2022-04-13 23:07:16 +0900

    SaveSystem

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private SaveSystem saveSystem;
+
+    private void Awake()
+    {
+        SceneManager.sceneLoaded += Initialize;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Initialize(Scene scene, LoadSceneMode sceneMode)
+    {
+        Debug.Log("Loaded GM");
+        if (saveSystem.LoadedData != null)
+        {
+            var damagable = player.GetComponentInChildren<Damagable>();
+            damagable.Health = saveSystem.LoadedData.playerHealth;
+        }
+    }
+}
---
+++ Assets/Scripts/Manager/SaveSystem.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class SaveSystem : MonoBehaviour
+{
+    public string playerHealthKey = "PlayerHealth";
+    public string sceneKey = "SceneKey";
+    public string savePresentKey = "PresentKey";
+
+    public LoadedData LoadedData { get; private set; }
+    public UnityEvent<bool> OnDataLoadedResult;
+    private bool isInitializes = false;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        var result = LoadData();
+        OnDataLoadedResult?.Invoke(result);
+         
+    }
+
+    public void ResetData()
+    {
+        PlayerPrefs.DeleteKey(playerHealthKey);
+        PlayerPrefs.DeleteKey(sceneKey);
+        PlayerPrefs.DeleteKey(savePresentKey);
+        LoadedData = null;
+    }
+    public bool LoadData()
+    {
+        if (PlayerPrefs.GetInt(savePresentKey) == 1)
+        {
+            LoadedData = new LoadedData();
+            LoadedData.playerHealth = PlayerPrefs.GetInt(playerHealthKey);
+            LoadedData.sceneIndex = PlayerPrefs.GetInt(sceneKey);
+            return true;
+        }
+        return false;
+    }
+
+    public void SaveData(int sceneIndex, int playerHealth)
+    {
+        if (LoadedData == null)
+            LoadedData = new LoadedData();
+        LoadedData.playerHealth = playerHealth;
+        LoadedData.sceneIndex = sceneIndex;
+        PlayerPrefs.SetInt(playerHealthKey, playerHealth);
+        PlayerPrefs.SetInt(sceneKey, sceneIndex);
+        PlayerPrefs.SetInt(savePresentKey, 1);
+
+    }
+}
+
+public class LoadedData
+{
+    public int playerHealth = -1;
+    public int sceneIndex = -1;
+}
